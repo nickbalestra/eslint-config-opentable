3039e2794e86d4c84030bc261a5b164907add18e
Nick Balestra <nbalestra@opentable.com>
2017-01-23 10:28:20 +0000

    eslint-config-opentable

--- src/loader.js
+++ src/loader.js
@@ -1,32 +1,37 @@
+/* eslint no-param-reassign: ["error", { "props": false }] */
 import lloader from 'little-loader';
 
 export function load(src, callback) {
-    if (typeof src === 'string') lloader(src, callback); else lloader(src.src, {
-        callback: callback || src.callback,
+  if (typeof src === 'string') lloader(src, callback);
+  else {
+    lloader(src.src, {
+      callback: callback || src.callback,
 
-        setup: function(script) {
-            script.crossOrigin = src.crossOrigin;
-        }
+      setup(script) {
+        script.crossOrigin = src.crossOrigin;
+      }
     });
+  }
 }
 
 export function parallel(libs, callback) {
-    var remaining = libs.length;
+  let remaining = libs.length;
+  const cb = callback || function () { };
 
-    callback = callback || function() { };
+  if (!remaining) cb();
+  else {
+    let done = false;
 
-    if (!remaining) callback(); else {
-        var done = false;
+    const loadCallback = (err) => {
+      remaining -= 1;
+      if (!done && (err || !remaining)) {
+        done = true;
+        cb(err);
+      }
+    };
 
-        for (var i = 0; i < libs.length; i++) {
-            load(libs[i], function(err) {
-                remaining--;
-
-                if (!done && (err || !remaining)) {
-                    done = true;
-                    callback(err);
-                }
-            });
-        }
+    for (let i = 0; i < libs.length; i += 1) {
+      load(libs[i], loadCallback);
     }
+  }
 }
